c26445b05af68f2c0a5ca5d062f72a51662b388a
Valentin-Ioan VINTILA <vintilavalentinioan@gmail.com>
2023-05-10 12:28:51 +0300

    Remove unnecessary comments

--- requests.hpp
+++ requests.hpp
@@ -1,18 +1,9 @@
 #ifndef _WI_REQUESTS_
 #define _WI_REQUESTS_
 
-#include <stdlib.h>     /* exit, atoi, malloc, free */
-#include <stdio.h>
-#include <unistd.h>     /* read, write, close */
-#include <string.h>     /* memcpy, memset */
-#include <sys/socket.h> /* socket, connect */
-#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
-#include <netdb.h>      /* struct hostent, gethostbyname */
-#include <arpa/inet.h>
-#include "helpers.h"
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
 
 using std::string;
 using std::vector;
@@ -22,8 +13,6 @@ using std::stringstream;
 
 // - - - - -
 
-// computes and returns a GET request string (query_params
-// and cookies can be set to NULL if not needed)
 string compute_get_request(
     string host, string url,
     string header, string cookie
@@ -36,7 +25,6 @@ string compute_get_request(
 
 // - - - - -
 
-// computes and returns a POST request string (cookies can be NULL if not needed)
 string compute_post_request(
     string host, string url, string content_type,
     vector<string> body_data, string header, string cookie
